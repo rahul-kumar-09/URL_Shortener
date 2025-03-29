3157226cd52338683a24ba6cac3bb8287631a374
rahul-kumar-09 <rahul7827759@gmail.com>
2025-03-29 19:54:55 +0530

    ui design

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const urlRoute = require('./routes/url');
 const {connectToMongoDb} = require('./connect')
 const URL = require('./models/url')
+const path = require('path')
+const staticRoute = require('./routes/staticRouter')
 
 const app = express();
 const PORT = 8000;
 
 connectToMongoDb("mongodb://127.0.0.1:27017/url-shortner").then(()=> console.log("mongodb connected"))
 
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use('/url', urlRoute);
+app.use('/', staticRoute)
+
+app.get('/url/test',async (req, res)=> {
+    const allURL = await URL.find({});
+    return res.render("home", {
+        urls: allURL,
+    })
+})
 
 app.get('/:shortId', async (req, res)=> {
     const shortId = req.params.shortId;
